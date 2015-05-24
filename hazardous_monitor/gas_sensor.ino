ca296897caf4585550f616bcfd02b0abdb9915e8
Igor Suleymanoff <igorsuleymanoff@gmail.com>
2015-05-24 20:33:26 +0300

    fix: removed old code

--- hazardous_monitor/gas_sensor.ino
+++
@@ -1,7 +0,0 @@
-#define voltage 4.95
-
-int read_co_volume() {
-  int sensor_value = analogRead(A0);
-  return (sensor_value / 1023 * voltage);
-}
-
--- hazardous_monitor/wireless.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define SSID "---------"
-#define PSWD "XXXXXXXXX"
-#define DST_IP "yyyyyyy"
-boolean connected = false;
-boolean deviceReady = false;
